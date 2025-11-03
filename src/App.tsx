2dd4560405b0fcd570e228f5ba47451db03783fe
juissi444-hub <juissi444@gmail.com>
2025-11-03 04:39:57 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RelationalFrameTrainer from './RelationalFrameTrainer';
+
+function App() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>Relational Frame Trainer</h1>
+      <RelationalFrameTrainer />
+    </div>
+  );
+}
+
+export default App;
